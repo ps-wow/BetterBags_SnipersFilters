7c716bc52838ae3f27436a14ccf645ee105da4ca
davidcraig <davidcraig-uk@protonmail.com>
2024-07-30 10:41:07 +0100

    Add more renown items.

--- dragonflight/renown.lua
+++ dragonflight/renown.lua
@@ -4,7 +4,13 @@ addon.dragonflight.renown = {
     
     -- 10.0 (Dragonflight)
     [200093] = true, -- Centaur Hunting Trophy (+50) [Maruuk Centaur]
-    [192055] = true, -- Dragon Isles Artifact (+45) [Dragonscale Expedition]
+    [192055] = true, -- Dragon Isles Artifact (+15) [Dragonscale Expedition]
+    [200443] = true, -- Dragon Isles Artifact (+15) [Dragonscale Expedition]
+    [200071] = true, -- Sacred Tuskarr Totem (+15) [Iskaara Tuskarr]
+    [200289] = true, -- Valdrakken Accord Insignia [Valdrakken Accord] [Account Bound]
+
+    -- 10.1 [Factions: Loamm Niffen]
+    [205989] = true, -- Symbol of Friendship [Loamm Niffen]
 
     -- 10.2 (Emerald Dream) [Factions: Dream Wardens]
     [211417] = true, -- Dream Wardens Insignia (+1000) [Dream Wardens]
