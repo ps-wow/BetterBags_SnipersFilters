1881da0a5f7ad7e1b6567ab5c5a93d8fb665a939
davidcraig <davidcraig-uk@protonmail.com>
2024-08-29 11:12:43 +0100

    Add more items.

--- warwithin/knowledge.lua
+++ warwithin/knowledge.lua
@@ -6,14 +6,19 @@ addon.warwithin.knowledge = {
     [226333] = true, -- Dornogal Chisel [+3]
     [226334] = true, -- Earthern Excavator's Shovel [+3]
     [226337] = true, -- Devout Archaeologist's Excavator [+3]
+    [224818] = true, -- Algari Miner's Notes [+3]
+    [224583] = true, -- Slab of Slate [+1]
 
     -- Herbalism
     [226301] = true, -- Dornogal Gardening Scythe [+3]
     [226302] = true, -- Earthen Digging Fork [+3]
+    [224264] = true, -- Deepgrove Petal [+1]
+    [226303] = true, -- Fungarian Slicer's Knife [+3]
 
     -- Skinning
     [226340] = true, -- Dornogal Carving Knife [+3]
     [226341] = true, -- Earthen Worker's Beams [+3]
+    [226347] = true, -- Carapace Shiner [+3]
 
     -- Alchemy
 
@@ -22,10 +27,12 @@ addon.warwithin.knowledge = {
 
 
     -- Engineering
+    [226293] = true, -- Dornogal Spectacles [+3]
 
 
     -- Enchanting
     [226285] = true, -- Silver Dornogal Rod [+3]
+    [227659] = true, -- Fleeting Arcane Manifestation [+1]
 
     -- Cooking
 
@@ -33,6 +40,7 @@ addon.warwithin.knowledge = {
     -- Leatherworking
     [226324] = true, -- Earthen Lacing Tools [+3]
     [226325] = true, -- Dornogal Craftsman's Flat Knife [+3]
+    [226331] = true, -- Curved Nerubian Skinning Knife [+3]
 
 
     -- Inscription
--- warwithin/openables.lua
+++ warwithin/openables.lua
@@ -2,4 +2,6 @@ local _, addon = ...
 
 addon.warwithin.openables = {
     [225249] = true, -- Rattling Bag o' Gold
+    [228959] = true, -- Pile of Unidentified Meat
+    [228361] = true, -- Seasoned Adventurer's Cache
 }
