16b4375bc6f9c1f665f83c670d0b1101eaebad94
davidcraig <davidcraig-uk@protonmail.com>
2024-09-04 11:41:40 +0100

    Add spark

--- warwithin/warwithin.lua
+++ warwithin/warwithin.lua
@@ -2,6 +2,8 @@ local _, addon = ...
 
 addon.warwithin = {
     sparks = {
+        -- Season 1
         [211297] = true, -- Fractured Spark of Omens
+        [211296] = true, -- Spark of Omens
     }
 }
